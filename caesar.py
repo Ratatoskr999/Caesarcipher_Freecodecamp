7402217156defe459397dd6d433d0fb98b2292e9
Sahan Imeshana <sahanmoradeniya@gmail.com>
2025-08-31 22:20:52 +0530

    Replace Caesar cipher with Vigenere ciphere

--- caesar.py
+++ caesar.py
@@ -1,16 +1,23 @@
-text = input("Enter your text here : ")
-alphabet ='abcdefghijklmnopqrstuvwxyz'
-shift =3
-encrypted =""
-for i in text.lower():
-   if i in alphabet:
-        
-     index = alphabet.find(i)
-     shifted=(index+shift)% len(alphabet)
-     encrypt = alphabet[shifted]
-   
-   else:
-     encrypt = i
-   encrypted += encrypt
+text = "Hello world"
+your_key = "python"
+def vigenere(message,key):
+ alphabet ='abcdefghijklmnopqrstuvwxyz'
+ encrypted =" "
+ key_index =0
 
-print("Your encrypted text :",encrypted)
+ for i in message.lower():
+   if i == " ":
+     encrypted += i
+   else:
+     key_char = key[key_index %len(key)]
+     offset = alphabet.find(key_char)
+     index = alphabet.find(i)
+     shifted=(index+offset) % len(alphabet)
+     encrypt = alphabet[shifted]
+     encrypted +=encrypt
+     key_index +=1
+    
+ return encrypted
+
+x =vigenere(text,your_key)
+print(x)
